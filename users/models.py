460b24de581e924f4cab6dd00463e8ff2c8ea0ba
RomeloDev <romelolucot1232@gmail.com>
2025-02-04 21:07:52 +0800

    users models have been created and mysql have been configured

--- users/models.py
+++ users/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, username, email, password=None, department=None):
+        if not email:
+            raise ValueError("Users must have an email address.")
+        if not username:
+            raise ValueError("Users must have a username")
+        
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, department=department)
+        user.set_password(password)
+        user.save(using=self.db)
+        return user
+    
+class User(AbstractBaseUser):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(unique=True, max_length=255)
+    password = models.CharField(max_length=255)
+    department = models.CharField(max_length=255, null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    objects = UserManager()
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+    
+    def __str__(self):
+        return self.username
